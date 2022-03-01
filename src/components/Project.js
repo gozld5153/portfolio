f97091bd9c8b6022bd36199fc5708e69789ac01d
gozld5153 <gozld5153@naver.com>
2022-03-01 20:43:51 +0900

    add css Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ProjectNbbang from "../img/nbbang.gif";
 import ProjectKickick from "../img/kickick.gif";
+import Observer from "./Observer.js";
 import { FaGithub } from "react-icons/fa";
 const Project = () => {
+  const [onNbbang, setOnNbbang] = useState(false);
+  const [onKickick, setOnKickick] = useState(false);
+
+  const handleNbbang = (isOn) => {
+    if (onNbbang) return;
+    if (isOn) setOnNbbang(true);
+  };
+  const handleKickick = (isOn) => {
+    if (onKickick) return;
+    if (isOn) setOnKickick(true);
+  };
+
   return (
     <Container id="project">
       <Title>Project</Title>
       <ItemBox>
         <Nbbang>
-          <ImgBox>
+          <ImgBox onNbbang={onNbbang}>
             <a href="https://nbbang.tk/" target="_blank" rel="noreferrer">
               <img src={ProjectNbbang} alt="" />
             </a>
           </ImgBox>
-          <Explanation>
+          <Explanation onNbbang={onNbbang}>
             <p>프로젝트: 엔빵(Nbbang)</p>
             <p>2주 4인 팀프로젝트</p>
             <p>협업 중 자신의 기여도를 알 수 있는 사이트</p>
@@ -40,15 +53,16 @@ const Project = () => {
               </a>
             </p>
           </Explanation>
+          <Observer callback={handleNbbang} margin="0px" />
         </Nbbang>
         <hr />
         <Kickkick>
-          <ImgBox>
+          <ImgBox2 onKickick={onKickick}>
             <a href="https://kickick.net/" target="_blank" rel="noreferrer">
               <img src={ProjectKickick} alt="" />
             </a>
-          </ImgBox>
-          <Explanation>
+          </ImgBox2>
+          <Explanation2 onKickick={onKickick}>
             <p>프로젝트: 킥킥(KicKick)</p>
             <p>4주 4인 팀프로젝트</p>
             <p>자신만의 비법(kick)을 공유하는 커뮤니티 사이트</p>
@@ -73,7 +87,8 @@ const Project = () => {
                 깃헙링크
               </a>
             </p>
-          </Explanation>
+          </Explanation2>
+          <Observer callback={handleKickick} margin="0px" />
         </Kickkick>
       </ItemBox>
     </Container>
@@ -133,12 +148,23 @@ const Nbbang = styled.div`
 `;
 const ImgBox = styled.div`
   width: 100%;
+  margin-left: ${({ onNbbang }) => (onNbbang ? "0" : "-10%")};
+  opacity: ${({ onNbbang }) => (onNbbang ? "1" : "0")};
+  transition: all 2s ease-in-out;
   @media only screen and (min-width: 1290px) {
     width: 45%;
   }
 `;
+
+const ImgBox2 = styled(ImgBox)`
+  margin-left: ${({ onKickick }) => (onKickick ? "0" : "-10%")};
+  opacity: ${({ onKickick }) => (onKickick ? "1" : "0")};
+  transition: all 1s ease-in-out;
+`;
 const Explanation = styled.div`
   width: 100%;
+  opacity: ${({ onNbbang }) => (onNbbang ? "1" : "0")};
+  transition: all 1.5s linear;
   @media only screen and (min-width: 1290px) {
     width: 45%;
   }
@@ -152,5 +178,9 @@ const Explanation = styled.div`
   svg {
     vertical-align: middle;
   }
+`;
+
+const Explanation2 = styled(Explanation)`
+  opacity: ${({ onKickick }) => (onKickick ? "1" : "0")};
 `;
 const Kickkick = styled(Nbbang)``;
